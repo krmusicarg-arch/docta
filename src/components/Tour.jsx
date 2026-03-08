d2a9629d78bbb75dcd7ec06369779d5f185788cd
krmusicarg <krmusicarg@gmail.com>
2026-03-08 12:23:53 -0300

    boxpoint

--- src/components/Tour.jsx
+++ src/components/Tour.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Tour = () => {
   const dates = [
     { date: 'Sabado 7', venue: 'La Pulperia', city: 'Córdoba, Carlos Paz', link: 'https://www.instagram.com/lapulperiavcp/' },
-    { date: 'Sabado 14', venue: 'Boca point, evento originarios', city: 'Córdoba, Santa Rosa 4200', link: 'https://www.instagram.com/originarios_band_of_brothers/' },
+    { date: 'Sabado 14', venue: 'Boxpoint, evento originarios', city: 'Córdoba, Santa Rosa 4200', link: 'https://www.instagram.com/originarios_band_of_brothers/' },
     { date: 'Viernes 27', venue: 'Cosquin', city: 'Cosquin', link: '' },
   ];
 
