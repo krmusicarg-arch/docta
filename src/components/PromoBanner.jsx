392e1e994d0109fd4ddb33b54d30f078b1a99988
krmusicarg <krmusicarg@gmail.com>
2026-03-24 10:36:17 -0300

    san patricio

--- src/components/PromoBanner.jsx
+++ src/components/PromoBanner.jsx
@@ -6,7 +6,7 @@ const PromoBanner = () => {
   const bannerRef = useRef(null); // Ref for the banner container
 
   const calculateTimeLeft = () => {
-    const eventDate = new Date('2026-03-21T22:00:00');
+    const eventDate = new Date('2026-03-28T21:00:00');
     const difference = +eventDate - +new Date();
     let newTimeLeft = {};
 
@@ -72,7 +72,7 @@ const PromoBanner = () => {
         <div className="promo-text">
           <p>🔥 ESTE SABADO EN VIVO 🔥</p>
           <p className="promo-details">
-            Docta Boss | 21 de Marzo | Rio Tercero | Alabama 
+            Docta Boss | 28 de Marzo | Cosquin | San Patricio 
           </p>
         </div>
         <div className="promo-countdown">
